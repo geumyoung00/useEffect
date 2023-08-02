f16fec7b503e9024f51f30387fdf59a4fb1e900b
geumyoung <1gyn093@gmail.com>
2023-08-02 19:18:03 +0900

    Combined enteredEmail, emailIsValid using useReducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
 const typingTimeout = () => console.log("ended typing");
+//useReducer는 상태값이 많아서 서로 엮여있을 때 주로 사용, 최신의 상태값을 확인해준다.
+
+const emailReducer = (state, action) => {
+  if (action.type === "INPUT_CHANGE") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
 
-  useEffect(() => {
-    const typingTimer = setTimeout(() => {
-      typingTimeout();
-    }, 1000);
-
-    return () => clearTimeout(typingTimer);
-  }, [enteredEmail, enteredPassword]);
-
-  useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
   });
 
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordIsValid, setPasswordIsValid] = useState();
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  // useEffect(() => {
+  //   const typingTimer = setTimeout(() => {
+  //     typingTimeout();
+  //   }, 1000);
+
+  //   return () => clearTimeout(typingTimer);
+  // }, [enteredEmail, enteredPassword]);
+
+  useEffect(() => {
+    setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6);
+  }, [emailState.isValid, enteredPassword]);
+
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    // setEnteredEmail(event.target.value);
+    dispatchEmail({ type: "INPUT_CHANGE", val: event.target.value });
     // setFormIsValid(
     //   event.target.value.includes("@") && enteredPassword.trim().length > 6
     // );
@@ -42,7 +59,9 @@ const Login = (props) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    // setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({ type: "INPUT_BLUR" });
+    // blur : focus가 안 된 상태
   };
 
   const validatePasswordHandler = () => {
@@ -51,7 +70,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -59,14 +78,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
